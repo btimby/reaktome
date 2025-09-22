31580c2e48b43d78980e4a2ee92a668b988fd74d
Ben Timby <btimby@gmail.com>
2025-09-22 15:06:57 -0400

    Initial implementation.

---
+++ test_reaktome.py
@@ -0,0 +1,44 @@
+import logging
+import unittest
+
+from typing import Any
+
+from unittest.mock import MagicMock
+from dataclasses import dataclass, field
+
+from reaktome import Reaktome, on
+
+
+LOGGER = logging.getLogger('reaktome')
+LOGGER.addHandler(logging.StreamHandler())
+LOGGER.setLevel(logging.DEBUG)
+
+
+@dataclass
+class TestDataClass(Reaktome):
+    name: str
+    price: int
+    items: list[int] = field(default_factory=list)
+    names: dict[str, Any] = field(default_factory=dict)
+
+
+class ReaktomeTestCase(unittest.TestCase):
+    def test_reaktome_singles(self):
+        handler0 = MagicMock()
+        handler1 = MagicMock()
+        top = TestDataClass('foo', 52)
+        top.on('name', handler0)
+        top.on('names[foo]', handler1)
+        top.name = 'bar'
+        top.names['foo'] = []
+        top.names['foo'].append('bar')
+        top.names['foo'].append('baz')
+        handler0.assert_called_once()
+        handler1.assert_called_once()
+
+    def test_reaktome_wildcard(self):
+        handler0 = MagicMock()
+        top = TestDataClass('foo', 42)
+        top.on('*', handler0)
+        top.name = 'bar'
+        handler0.assert_called_once_with('name', 'foo', 'bar')
