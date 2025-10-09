889f8c00e1a26d173be225af3a30181c53435243
btimby <btimby@users.noreply.github.com>
2025-10-09 18:41:22 +0000

    v0.1.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension, find_packages
 
 setup(
     name="reaktome",
-    version="0.1.12",
+    version="0.1.13",
     description="Advisory-only setattr hooks with veto support",
     packages=find_packages(include=["reaktome", "reaktome.*"]),
     package_data={
