c7669906e2c8af187ddb69cd9a767819d661390e
Ben Timby <btimby@gmail.com>
2025-10-28 18:43:39 -0400

    Provide linking instructions, for musl compat.

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, Extension, find_packages
+import sysconfig
+
+python_ldflags = sysconfig.get_config_var("LDFLAGS") or ""
+python_libs = sysconfig.get_config_var("LIBS") or ""
+python_ldlibrary = sysconfig.get_config_var("LDLIBRARY") or ""
+python_libdir = sysconfig.get_config_var("LIBDIR") or ""
+python_ldargs = [flag for flag in python_ldflags.split() if flag]
 
 setup(
     name="reaktome",
@@ -20,7 +27,9 @@ setup(
                 "src/activation.c",
             ],
             include_dirs=["src"],  # <— tells gcc where to find reaktome.h
-        )
+            library_dirs=[python_libdir] if python_libdir else [],
+            extra_link_args=python_ldargs + [f"-lpython{sysconfig.get_python_version()}"],
+        ),
     ],
     python_requires=">=3.12",
 )
